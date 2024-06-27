d376b929b864c4440ad0f43fc7be3215c69988a5
ew750x <144366894+ew750x@users.noreply.github.com>
2024-06-27 10:53:00 -0400

    Add files via upload

---
+++ portal/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+WORK_DIR=/usr/share/nginx/html/qujata
+
+
+case ${1} in
+  app:start)
+    echo "----------------------------------------------------------------"
+    echo "---------------${1} PQC_PORTAL Starting... -------------------"
+    echo "----------------------------------------------------------------"
+    node -v
+    # Injecting Environment Variables
+    node ${WORK_DIR}/inject-env-variables.js $WORK_DIR
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
+
+chmod -R 755 /usr/share/nginx/html/qujata
+nginx -g 'daemon off;'
