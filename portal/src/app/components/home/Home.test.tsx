d8266c789650c4502a3c32260e76518751b1c846
ew750x <144366894+ew750x@users.noreply.github.com>
2024-06-27 11:16:14 -0400

    Add files via upload

---
+++ portal/src/app/components/home/Home.test.tsx
@@ -0,0 +1,70 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { Home } from './Home';
+import { SubHeader, SubHeaderProps } from '../sub-header';
+import { ProtocolQuery, ProtocolQueryProps } from '../protocol-query';
+
+const mockUseNavigate = jest.fn();
+const mockUseLocation = jest.fn();
+
+jest.mock('../sub-header');
+jest.mock('../protocol-query');
+jest.mock('../../hooks/useDashboardData', () => ({
+  useDashboardData: () => ({
+    handleRunQueryClick: jest.fn(),
+    link: 'initialLink',
+    status: 'idle',
+  }),
+}));
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+  useLocation: () => mockUseLocation,
+}));
+
+describe('Home', () => {
+    test('should render Home', async () => {
+      (SubHeader as jest.Mock).mockImplementation((props: SubHeaderProps) => {
+        function onClick() {
+          props.handleCloseClick();
+        }
+        return <div onClick={onClick} data-testid='submit-id'>SubHeader</div>;
+      });
+      (ProtocolQuery as jest.Mock).mockImplementation(() => <div>ProtocolQuery</div>);
+      const { container, getByTestId } = render(<Home />);
+      const submitButtonElement: HTMLElement = getByTestId('submit-id');
+
+      await waitFor(() => {
+        expect(container).toBeTruthy();
+      });
+
+      await waitFor(() => {
+        fireEvent.click(submitButtonElement);
+      });
+    });
+
+    test('should click on run button', async () => {
+      (ProtocolQuery as jest.Mock).mockImplementation((props: ProtocolQueryProps) => {
+        function onClick() {
+          props.onRunClick({ 
+            experimentName: 'test',
+            algorithms: { label: 'regular', value: 'regular' },
+            iterationsCount: { label: 'regular', value: 'regular' },
+            messageSizes: [{ label: '1024', value: '1024' }],
+            description: 'test'
+          });
+        }
+        return <div onClick={onClick} data-testid='submit-id'>SubHeader</div>;
+      });
+      (SubHeader as jest.Mock).mockImplementation(() => <div>SubHeader</div>);
+      const { container, getByTestId } = render(<Home />);
+      const submitButtonElement: HTMLElement = getByTestId('submit-id');
+
+      await waitFor(() => {
+        expect(container).toBeTruthy();
+      });
+
+      await waitFor(() => {
+        fireEvent.click(submitButtonElement);
+      });
+    });
+});
---
+++ portal/src/app/components/home/Home.tsx
@@ -0,0 +1,60 @@
+import { IUseDashboardData, useDashboardData } from "../../hooks/useDashboardData";
+import { FetchDataStatus } from "../../shared/hooks/useFetch";
+import { ITestParams } from "../../shared/models/quantum.interface";
+import { ProtocolQuery } from "../protocol-query";
+import { SubHeader } from "../sub-header";
+import { useCallback, useEffect, useState } from 'react';
+import styles from './Home.module.scss';
+import { useLocation, useNavigate } from "react-router-dom";
+import { ExperimentData } from "../all-experiments/models/experiments.interface";
+
+export const Home: React.FC = () => {
+    const [isSubHeaderOpen, setIsSubHeaderOpen] = useState<boolean>(true);
+  
+    const handleSubHeaderCloseClick: () => void = useCallback((): void => {
+      setIsSubHeaderOpen(false);
+    }, []);
+    
+    return (
+      <>
+        {isSubHeaderOpen && <SubHeader handleCloseClick={handleSubHeaderCloseClick} />}
+        <HomeContent />
+      </>
+    );  
+}
+
+export const HomeContent: React.FC = () => {
+  const { handleRunQueryClick, status, testSuiteId }: IUseDashboardData = useDashboardData();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [duplicateData, setDuplicateData] = useState<ExperimentData | undefined>(location.state?.row);
+
+  useEffect(() => {
+    // Clear the state after the duplicate data has been created
+    setDuplicateData(undefined);
+  }, []);
+
+  useEffect(() => {
+    if (status === FetchDataStatus.Success && testSuiteId) {
+      // Navigate to the Experiment page
+      navigate(`experiment/${testSuiteId}`,  { replace: true });
+    }
+  }, [navigate, status, testSuiteId]);
+  
+  const handleRunClick: (params: ITestParams) => void = useCallback((params: ITestParams): void => {
+    if (params.experimentName && params.algorithms && params.iterationsCount) {
+      handleRunQueryClick(params);
+    }
+  }, [handleRunQueryClick]);
+
+  return (
+    <div className={styles.app_wrapper}>
+      <ProtocolQuery
+        isFetching={status === FetchDataStatus.Fetching}
+        onRunClick={handleRunClick}
+        duplicateData={duplicateData}
+        setDuplicateData={setDuplicateData}
+      />
+    </div>
+  );
+};
